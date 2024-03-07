f8f6383a992cee7190309da2371d6cc9e57e964e
leehyeyeong <elto789@naver.com>
2024-03-07 19:34:45 +0900

    feat: 상품목록 api 연결

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,17 +1,45 @@
+import { changeUrl } from "./main.js";
+
+const APIURL = "http://localhost:8080/products"
+export async function getList() {
+  try {
+    const res = await fetch(APIURL, {
+      method: "GET",
+    });
+    const json = await res.json();
+    return json;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const listData = await getList();
+
+window.addEventListener("click", (e) => {
+  if (e.target.classList.contains("Product_img")) {
+    // 이미지가 클릭된 경우 
+    const product_id = e.target.getAttribute('id');
+    changeUrl(`/products/${product_id}`);
+    console.log(product_id);
+  }
+});
+
 class Home {
   template() {
     return `
     <div class="ProductListPage">
       <h1>상품목록</h1>
       <ul>
-        <li class="Product">
-          <img class="Product_img"
-            src="https://grepp-cloudfront.s3.ap-northeast-2.amazonaws.com/programmers_imgs/assignment_image/cafe_coffee_cup.png">
-          <div class="Product__info">
-            <div>커피잔</div>
-            <div>10,000원~</div>
-          </div>
-        </li>
+      ${listData.map((data) => `
+      <li class="Product">
+      <img class="Product_img" id=${data.id}
+        src=${data.imageUrl}>
+      <div class="Product__info">
+        <div>${data.name}</div>
+        <div>${data.price}</div>
+      </div>
+    </li>
+      `).join("")}
       </ul>
     </div>
     `
