11a10e9ab7105679efcc35ba7d414ca81a931861
OhHyukJun <kmh7277@naver.com>
2024-03-09 18:19:21 +0900

    Feat: get api

---
+++ client/src/api.js
@@ -0,0 +1,14 @@
+const baseUrl = 'http://localhost:8080';
+
+const api = {
+  async getProducts() {
+    const res = await fetch(baseUrl + '/products');
+    return await res.json();
+  },
+  async getProduct(id) {
+    const res = await fetch(baseUrl + `/products/${id}`);
+    return await res.json();
+  },
+};
+
+export default api;
